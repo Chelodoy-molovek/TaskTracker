cbc4f00b745a399989daf7bb51aaa315153e2f15
Islam Alaev <alaev095@bk.ru>
2021-04-12 18:18:43 +0300

    Html&CssRe

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('slam');
+let inputText = document.querySelector('.inputText')
+let buttonAdd = document.querySelector('.buttonAdd');
+let container = document.querySelector('.container');
+let deleteEl = document.querySelector('.deleteButton');
+let text = document.querySelector('.text');
+
+// deleteEl.addEventListener('click', deleteDiv);
+
+// function deleteDiv(event) {
+//     event.target.parentElement.parentElement.remove();
+// }
+
+// buttonAdd.addEventListener('click', slam1);
+
+// function slam1() {
+//     let olo = inputText.cloneNode(true);
+//     container.insertBefore(olo, buttonAdd);
+//     let pop = olo.querySelector('.deleteButton');
+//     pop.addEventListener('click', deleteDiv);
+
+// }
+
+// buttonAdd.addEventListener('click', dobavlenie);
+
+// function dobavlenie() {
+//     let str = text.value; // получаем введенный текст
+//     let newDiv = document.createElement('div'); // создаем новый див
+//     let newP = document.createElement('p'); // оздаем новый параграф
+//     newP.textContent = str; // добавляем в параграф текс из инпута
+//     newDiv.append(newP); // создаем переменную в которой див, в которую добали параграф
+//     container.append(newDiv);
+//     newDiv.classList.add('lolo')
+// }
